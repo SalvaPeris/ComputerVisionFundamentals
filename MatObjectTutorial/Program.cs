195bb4f90d3d6a66dd91b4bf20a0642b87690dce
SalvaPeris <sperisgimeno@gmail.com>
2024-11-28 10:30:44 +0100

    Finished MatObjectTutorial.

--- MatObjectTutorial/Program.cs
+++ MatObjectTutorial/Program.cs
@@ -5,8 +5,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        string path = @"..\..\..\..\images\lena.jpg";
+        Mat colorImage = Cv2.ImRead(path, ImreadModes.Color);
+
+        //4.2 Mat Object creation
         //CreateMatObject();
-        CopyMatObject();
+
+        //4.3. Copy and clone Mat objects
+        //CopyMatObject(colorImage);
+
+        //RoiMatObject(colorImage);
+
+        //4.5 Mat Object Image manipulation
+        //GetPixels(colorImage);
+
+        //4.6 Mat Object Image manipulation - Second Method
+        //GetPixelsIndexer(colorImage);
+
+        //4.6.2 Mat Object Image manipulation - Third Method
+        // MatOfByte3 - Object not found in library
 
         Cv2.WaitKey();
         Cv2.DestroyAllWindows();
@@ -21,10 +38,8 @@ internal class Program
         Cv2.ImShow("m2", mat2);
     }
 
-    private static void CopyMatObject()
+    private static void CopyMatObject(Mat colorImage)
     {
-        string path = @"..\..\..\..\images\lena.jpg";
-        Mat colorImage = Cv2.ImRead(path, ImreadModes.Color);
         Cv2.ImShow("original", colorImage);
 
         Mat clonedImage = colorImage.Clone();
@@ -33,5 +48,68 @@ internal class Program
         Mat anotherClonedImage = new Mat();
         colorImage.CopyTo(anotherClonedImage);
         Cv2.ImShow("anotherCloned", anotherClonedImage);
+    }
+
+    private static void RoiMatObject(Mat colorImage)
+    {
+        Mat[] channels;
+        Cv2.Split(colorImage, out channels);
+        Cv2.ImShow("Blue", channels[0]);
+        Cv2.ImShow("Green", channels[1]);
+        Cv2.ImShow("Red", channels[2]);
+        Mat roiImage = new Mat(colorImage, new Rect(50, 50, 250, 250));
+        Cv2.ImShow("roi", roiImage);
+    }
+
+    private static void GetPixels(Mat colorImage)
+    {
+        int numRows = colorImage.Rows;
+        int numCols = colorImage.Cols;
+        Mat cImage = colorImage.Clone();
+
+        for (int y = 0; y < numRows; y++)
+        {
+            for (int x = 0; x < numCols; x++)
+            {
+                Vec3b pixel = cImage.Get<Vec3b>(y, x);
+                byte blue = pixel.Item0;
+                byte red = pixel.Item2;
+                byte green = pixel.Item1;
+                byte temp = blue;
+                pixel.Item0 = red;
+                pixel.Item2 = temp;
+
+                cImage.Set<Vec3b>(y, x, pixel);
+            }
+        }
+
+        Cv2.ImShow("swapped", cImage);
+    }
+
+    private static void GetPixelsIndexer(Mat colorImage)
+    {
+        int numRows = colorImage.Rows;
+        int numCols = colorImage.Cols;
+        Mat cImage = colorImage.Clone();
+
+        var indexer = cImage.GetGenericIndexer<Vec3b>();
+
+        for (int y = 100; y < numRows - 100; y++)
+        {
+            for (int x = 100; x < numCols - 100; x++)
+            {
+                Vec3b pixel = indexer[y, x];
+                byte blue = pixel.Item0;
+                byte red = pixel.Item2;
+                byte green = pixel.Item1;
+                byte temp = blue;
+                pixel.Item0 = red;
+                pixel.Item2 = temp;
+
+                indexer[y, x] = pixel;
+            }
+        }
+
+        Cv2.ImShow("swapped", cImage);
     }
 }
