bffbd32591404ecc7f6f0f9b10d61d666318ef80
SalvaPeris <sperisgimeno@gmail.com>
2024-12-10 12:47:24 +0100

    Added Chapter 6.2 Part 1.

--- Chapter6/ThresholdingDemo/Program.cs
+++ Chapter6/ThresholdingDemo/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
+﻿using OpenCvSharp;
 
+internal class Program
+{
+    private void Main(string[] args)
+    {
+        string path = @"..\..\..\..\..\images\lena.jpg";
+        Mat image = Cv2.ImRead(path, ImreadModes.Color);
+        Cv2.ImShow("original", image);
+
+        GetBinaryThreshold(image);
+
+        Cv2.WaitKey();
+        Cv2.DestroyAllWindows();
+    }
+
+    private void GetBinaryThreshold(Mat image)
+    {
+        Mat threshold = new Mat(new Size(image.Width, image.Height), MatType.CV_8UC3, new Scalar(0));
+        Cv2.Threshold(image, threshold, 15, 255, ThresholdTypes.Binary);
+
+        Cv2.ImShow("threshold", threshold);
     }
 }
